9dc1739534b203c4fc6dfdbd9516f01d4736848f
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2022-06-16 14:16:21 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,34 @@
 let on = 0
-let dogonMat = 0
-let Urgency = 0
 let DogatDoor = 0
+let dogonMat = 0
 input.onButtonPressed(Button.A, function () {
     on = 1
 })
 input.onButtonPressed(Button.B, function () {
     on = 0
+    DogatDoor = 0
 })
 basic.forever(function () {
     while (on == 1) {
         dogonMat = pins.analogReadPin(AnalogPin.P0)
-        Urgency = 0
+        basic.showNumber(pins.analogReadPin(AnalogPin.P0))
         DogatDoor = 0
-        if (input.soundLevel() > 200 || dogonMat > 500) {
-            Urgency += 1
-            basic.showLeds(`
-                # . . . .
-                # . . # #
-                # # # # #
-                . # # # .
-                # . . . #
-                `)
-            basic.pause(1000)
-            music.playTone(262, music.beat(BeatFraction.Double))
-            basic.clearScreen()
+        if (dogonMat > 500) {
+            DogatDoor += 1
+            for (let index = 0; index < 2; index++) {
+                music.playTone(262, music.beat(BeatFraction.Double))
+                basic.showLeds(`
+                    # . . . .
+                    # . . # #
+                    # # # # #
+                    . # # # .
+                    # . . . #
+                    `)
+                basic.pause(1000)
+                basic.clearScreen()
+            }
         } else {
-        	
+            basic.showString("System Armed")
         }
     }
 })
